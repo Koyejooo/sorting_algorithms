a29d530ee3b42b1b53fc4ae58af104c22931c4e7
Koyejo <adinlewakoyejo@yahoo.com>
2023-07-22 08:27:48 -0700

    Added a func to sort an array of ints with selection algo

---
+++ 2-selection_sort.c
@@ -0,0 +1,45 @@
+#include "sort.h"
+
+/**
+ * swap_ints - Swap two integers in an array.
+ * @a: Incoming argument for first integer to be swapped.
+ * @b: Incoming argument for the second integer to be swapped.
+ */
+void swap_ints(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+/**
+ * selection_sort - Sort an array of integers in ascending order
+ *                  using the selection sort algorithm.
+ * @array: Incoming argument for an array of integers, to be sorted.
+ * @size: Incoming argument for the size of the array to be sorted.
+ *
+ * Description: Prints the array after each swap.
+ */
+void selection_sort(int *array, size_t size)
+{
+	int *min;
+	size_t i, j;
+
+	if (array == NULL || size < 2)
+		return;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min = array + i;
+		for (j = i + 1; j < size; j++)
+			min = (array[j] < *min) ? (array + j) : min;
+
+		if ((array + i) != min)
+		{
+			swap_ints(array + i, min);
+			print_array(array, size);
+		}
+	}
+}
